b980f89f94fe65023164b7993244a1ff3542fae0
augustan-dev <augustan.dev@gmail.com>
2024-01-23 23:53:03 +0300

    update

--- components/Admin/SizeOptions.js
+++ components/Admin/SizeOptions.js
@@ -17,15 +17,6 @@ export default function ColourOptions() {
 			{sizes.numbers.map((value) => (
 			<SizeInput value={value} key={Math.rand}></SizeInput>
 			))}
-
-			{/* <SizeInput value="3XL"></SizeInput>
-			<SizeInput value="2XL"></SizeInput>
-			<SizeInput value="XXL"></SizeInput>
-			<SizeInput value="L"></SizeInput>
-			<SizeInput value="M"></SizeInput>
-			<SizeInput value="S"></SizeInput>
-			<SizeInput value="XS"></SizeInput>
-			<SizeInput value="XXS"></SizeInput> */}
 		</div>
 	);
 }
